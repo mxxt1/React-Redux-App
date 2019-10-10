99e7cb017ac7aa43aa7445f0fd1820256ed8ee35
mxxt1 <matt@dustcloudlabs.com>
2019-10-10 18:57:46 -0400

    MVP - added basic styling

--- react-redux-app/src/components/TriviaList.js
+++ react-redux-app/src/components/TriviaList.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import {connect} from 'react-redux';
 import TriviaCard from './TriviaCard';
+import styled from 'styled-components';
 
+
+const CardContainers = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+    border: 2px solid black;
+`;
 
 const TriviaList = (props) => {
     console.log(props);
     
     return (
-        <div>
+        <CardContainers>
         {props.trivia.map((item,index) => (
             <TriviaCard key={index} item={item} />
         ))} 
-        </div>
+        </CardContainers>
     )
 }
 
