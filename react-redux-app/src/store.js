8f7b851b0f77dd7a48b91b2dc98f3df2d4a1638e
mxxt1 <matt@dustcloudlabs.com>
2019-10-10 16:28:04 -0400

    init

---
+++ react-redux-app/src/store.js
@@ -0,0 +1,12 @@
+import {createStore, compose, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middleware))
+);
